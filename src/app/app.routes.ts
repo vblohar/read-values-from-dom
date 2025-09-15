73497cafc92c7f92ff65045a861e9110bcccf9e9
Vaibhav L <loharvb@gmail.com>
2025-09-15 17:01:15 +0530

    Added routes and lazy loading functionality..

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { TwoWayBinding } from './components/two-way-binding/two-way-binding';
+import { EventBinding } from './components/event-binding/event-binding';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // {
+    //     path: '', component: App
+    // },
+    {
+        path: 'twoBinding', component: TwoWayBinding
+    },
+    {
+        path: 'eventBinding', component: EventBinding
+    },
+    {
+        path: 'viewReference', loadComponent:()=> import('./components/template-reference-variable/template-reference-variable').then(c=> c.TemplateReferenceVariable)
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { TemplateReferenceVariable } from "./components/template-reference-variable/template-reference-variable";
 import { TwoWayBinding } from "./components/two-way-binding/two-way-binding";
 import { ViewChildAndRenderer2WithElementRef } from "./components/view-child-and-renderer2-with-element-ref/view-child-and-renderer2-with-element-ref";
@@ -7,9 +7,11 @@ import { EventBinding } from "./components/event-binding/event-binding";
 import { ViewChildrenQuerylistElementref } from "./components/view-children-querylist-elementref/view-children-querylist-elementref";
 import { ReactiveFormsFormControlFormGroup } from "./components/reactive-forms-form-control-form-group/reactive-forms-form-control-form-group";
 
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TemplateReferenceVariable, TwoWayBinding, ViewChildAndRenderer2WithElementRef, EventBinding, ViewChildrenQuerylistElementref, ReactiveFormsFormControlFormGroup],
+  imports: [RouterOutlet, TwoWayBinding, ViewChildAndRenderer2WithElementRef, 
+    EventBinding, ViewChildrenQuerylistElementref, ReactiveFormsFormControlFormGroup, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
